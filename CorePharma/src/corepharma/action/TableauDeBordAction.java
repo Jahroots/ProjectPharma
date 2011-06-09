d9238987c47125768063bf1cb031da936cabdcd9
ramel.adjibi@gmail.com <ramel.adjibi@gmail.com@d9b094d0-503d-e482-3bb6-2bad77c90a7b>
2011-06-09 21:02:38 +0000

    Configuration du fichier log4j properties. Ajout d'une personnalisation de méthode SUCCESS pour l'action "tableau_de_bord"

--- CorePharma/src/corepharma/action/TableauDeBordAction.java
+++ CorePharma/src/corepharma/action/TableauDeBordAction.java
@@ -1,26 +1,33 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package corepharma.action;
 import org.apache.log4j.Logger;
+import com.opensymphony.xwork2.ActionSupport;
 
 /**
  *
  * @author radjibi
  */
-public class TableauDeBordAction {
+public class TableauDeBordAction extends ActionSupport {
+
+	private static final long serialVersionUID = -8210091523952012797L;
+	private static final Logger logger = Logger.getLogger(TableauDeBordAction.class);
     
-    private static final Logger logger = Logger.getLogger(TableauDeBordAction.class);
     public TableauDeBordAction(){}
     
-    public String execute()
+    public String afficheTableauDeBord()
     {
+        //Entrée de l'action
         if(logger.isDebugEnabled())
+            logger.debug("Call Action ---> TableauDeBordAction");
         
-		logger.info("Call Action ---> TableauDeBordAction");
-	}
-        return "SUCCESS";
+        //Information quelconque à logger
+        if(logger.isDebugEnabled())
+            logger.debug("Essai de log");
+        
+        //Sortie de l'action
+        if(logger.isDebugEnabled())
+            logger.debug("Call Action <--- TableauDeBordAction OK");
+        
+        return SUCCESS;
     }
     
 }
